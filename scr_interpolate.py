39f2f49895ed628b8b7904582e7e3b6cdeb34867
Jussi Tiira <jussi.tiira@helsinki.fi>
2016-10-28 15:52:16 +0300

    towards interpolating data sequences

--- scr_interpolate.py
+++ scr_interpolate.py
@@ -7,11 +7,14 @@ import netCDF4 as nc
 import os
 #import pandas as pd
 #import h5py
-from pyoptflow import utils, visualization
+from pyoptflow import utils
 from pyoptflow.core import extract_motion_proesmans
 from pyoptflow.interpolation import interpolate
 import matplotlib.pyplot as plt
 import numpy as np
+import glob
+import copy
+import itertools
 #import pyart
 #import grid_io_withradx2gridread as gio
 
@@ -59,42 +62,85 @@ def ncdatetime(ncdata):
     times = ncdata.variables['time']
     return nc.num2date(times[:], times.units)
 
-def read_testfile(filename):
-    testfilepath = os.path.join(testpath, filename)
-    return nc.Dataset(testfilepath, 'r')
-    
+def discard(filepath, ncdata):
+    """Return True if the file was discarded."""
+    datadir = os.path.dirname(filepath)
+    discardir = os.path.join(datadir, 'discarded')
+    ensure_path(discardir)
+    ncdata.close()
+    discardfilepath = os.path.join(discardir, os.path.basename(filepath))
+    os.rename(filepath, discardfilepath)
+
+def data_is_bad(ncdata):
+    z = ncdata.variables['z0']
+    not_ppi = False # TODO
+    no_kdp = 'KDP' not in list(ncdata.variables)
+    high_elevation = z[0] > 1.5
+    return no_kdp or high_elevation or not_ppi
+
+def filter_filepaths(filepaths_all):
+    filepaths_good = copy.deepcopy(filepaths_all)
+    for filepath in filepaths_all:
+        with nc.Dataset(filepath, 'r') as ncdata:
+            if data_is_bad(ncdata):
+                print('bad: ' + filepath)
+                filepaths_good.remove(filepath)
+    return filepaths_good
+
+def work(filepaths_all):
+    pass
+
 testpath = '/media/jussitii/04fafa8f-c3ca-48ee-ae7f-046cf576b1ee/test'
+testfilepaths = glob.glob(os.path.join(testpath, '???', '*.nc'))
+testfilepaths.sort()
 gridpath = '/media/jussitii/04fafa8f-c3ca-48ee-ae7f-046cf576b1ee/grid'
 
-filename = 'ncf_20160903_070054.nc'
-filename0 = 'ncf_20160904_033827.nc'
-filename1 = 'ncf_20160904_033958.nc'
-nc0 = read_testfile(filename0)
-nc1 = read_testfile(filename1)
+d_filepaths_all = {}
+for site in ['KUM', 'KER', 'VAN']:
+    d_filepaths_all[site] = glob.glob(os.path.join(gridpath, site, '*', '*.nc'))
 
-datafilepath = os.path.join(gridpath, 'KUM', '20160903', filename)
-datadir = os.path.dirname(datafilepath)
-ncdata = nc.Dataset(datafilepath, 'r')
+d_filepaths_good = copy.deepcopy(d_filepaths_all)
+for site in d_filepaths_all:
+    pass
 
-# Discard bad files
-discardir = os.path.join(datadir, 'discarded')
-ensure_path(discardir)
-if 'KDP' not in list(ncdata.variables):
-    ncdata.close()
-    discardfilepath = os.path.join(discardir, os.path.basename(datafilepath))
-    os.rename(datafilepath, discardfilepath)
-    #continue
+testfilepaths_good = filter_filepaths(testfilepaths)
+dt = []
+for f0, f1 in itertools.izip(testfilepaths_good, testfilepaths_good[1:]):
+    nc0 = nc.Dataset(f0, 'r')
+    nc1 = nc.Dataset(f1, 'r')
+    I1 = nc_r(nc0)
+    I2 = nc_r(nc1)
+    t0 = ncdatetime(nc0)[0]
+    t1 = ncdatetime(nc1)[0]
+    dt.append(t1-t0)
+    #interpd = interp(I1, I2)
 
-I1 = nc_r(nc0)
-I2 = nc_r(nc1)
+#filename0 = 'ncf_20160904_033827.nc' # KUM
+#filename1 = 'ncf_20160904_033958.nc' # KUM, dt=91s
+#filename0 = 'ncf_20160904_033918.nc' # KER
+#filename1 = 'ncf_20160904_034208.nc' # KER, dt=170s
 
-interpd = interp(I1, I2)
-plot_rainmap(I1)
-plot_rainmap(interpd[0])
-plot_rainmap(I2)
-
-t0 = ncdatetime(nc0)[0]
-t1 = ncdatetime(nc1)[0]
-dt = t1-t0
-
-#gdata = gio.read_radx_grid(datafilepath)
+def testcase():
+    filename0 = 'ncf_20160904_034033.nc' # VAN
+    filename1 = 'ncf_20160904_034056.nc' # VAN, dt=23s
+    filepath0 = os.path.join(testpath, filename0)
+    filepath1 = os.path.join(testpath, filename1)
+    nc0 =  nc.Dataset(filepath0, 'r')
+    nc1 =  nc.Dataset(filepath1, 'r')
+    
+    #datafilepath = os.path.join(gridpath, 'KUM', '20160903', filename0)
+    #ncdata = nc.Dataset(datafilepath, 'r')
+    I1 = nc_r(nc0)
+    I2 = nc_r(nc1)
+    
+    interpd = interp(I1, I2)
+    plot_rainmap(I1)
+    for rmap in interpd:
+        plot_rainmap(rmap)
+    plot_rainmap(I2)
+    
+    t0 = ncdatetime(nc0)[0]
+    t1 = ncdatetime(nc1)[0]
+    dt = t1-t0 
+    
+    #gdata = gio.read_radx_grid(datafilepath)
