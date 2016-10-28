41c23e9fe9e25fd3f6a9c983f0733eff3980f484
Jussi Tiira <jussi.tiira@helsinki.fi>
2016-10-28 11:02:14 +0300

    interpolation working

--- delete_extra.py
+++ delete_extra.py
@@ -15,29 +15,27 @@ import numpy as np
 #import pyart
 #import grid_io_withradx2gridread as gio
 
-def interp(I1, I2, n=10):
+def interp(I1, I2, n=1):
     """Interpolate n frames."""
     VF,VB = motion(I1, I2)
-    # Interpolate ten frames between the input images and plot them.
+    # Interpolate n frames between the input images.
     return interpolate(I1, I2, VF, n, VB=VB)
 
 def motion(I1, I2):
-    # Threshold out near-zero rainfall intensities.
-    I1_ = I1.copy()
-    I1_[I1 < 0.05] = np.nan
-    I2_ = I2.copy()
-    I2_[I2 < 0.05] = np.nan
     # Convert the rainfall maps to unsigned byte, as required by the Optflow 
     # motion detection algorithms. Gaussian filter with std. dev. 3 is applied.
-    I1 = utils.rainfall_to_ubyte(I1, R_min=0.05, R_max=10.0, filter_stddev=3.0)
-    I2 = utils.rainfall_to_ubyte(I2, R_min=0.05, R_max=10.0, filter_stddev=3.0)
+    Iu = []
+    for i, I in enumerate([I1, I2]):
+        Iu.append(utils.rainfall_to_ubyte(I, R_min=0.05, R_max=10.0, filter_stddev=3.0))
     # Compute the motion field by using the Python <-> C++ API and the Proesmans 
     # algorithm.
-    return extract_motion_proesmans(I1, I2, lam=25.0, num_iter=250, num_levels=6)
+    return extract_motion_proesmans(Iu[0], Iu[1], lam=25.0, num_iter=250, num_levels=6)
 
 def plot_rainmap(r):
+    r_ = r.copy()
+    r_[r < 0.05] = np.nan
     plt.figure()
-    plt.imshow(r, vmin=0.05, vmax=10)
+    plt.imshow(r_, vmin=0.05, vmax=10)
     cb = plt.colorbar()
     cb.set_label("rain rate (mm/h)")
     plt.xticks([])
@@ -60,24 +58,27 @@ def ensure_path(directory):
 def ncdatetime(ncdata):
     times = ncdata.variables['time']
     return nc.num2date(times[:], times.units)
+
+def read_testfile(filename):
+    testfilepath = os.path.join(testpath, filename)
+    return nc.Dataset(testfilepath, 'r')
     
 testpath = '/media/jussitii/04fafa8f-c3ca-48ee-ae7f-046cf576b1ee/test'
 gridpath = '/media/jussitii/04fafa8f-c3ca-48ee-ae7f-046cf576b1ee/grid'
 
 filename = 'ncf_20160903_070054.nc'
-#filename1 = 'ncf_20160903_070311.nc'
 filename0 = 'ncf_20160904_033827.nc'
-filename1 = 'ncf_20160904_034311.nc'
-testfilepath0 = os.path.join(testpath, filename0)
-testfilepath1 = os.path.join(testpath, filename1)
-nc0 = nc.Dataset(testfilepath0)
-nc1 = nc.Dataset(testfilepath1)
+filename1 = 'ncf_20160904_033958.nc'
+nc0 = read_testfile(filename0)
+nc1 = read_testfile(filename1)
+
 datafilepath = os.path.join(gridpath, 'KUM', '20160903', filename)
 datadir = os.path.dirname(datafilepath)
 ncdata = nc.Dataset(datafilepath, 'r')
+
+# Discard bad files
 discardir = os.path.join(datadir, 'discarded')
 ensure_path(discardir)
-
 if 'KDP' not in list(ncdata.variables):
     ncdata.close()
     discardfilepath = os.path.join(discardir, os.path.basename(datafilepath))
@@ -87,8 +88,13 @@ if 'KDP' not in list(ncdata.variables):
 I1 = nc_r(nc0)
 I2 = nc_r(nc1)
 
-r = nc_r(ncdata)
+interpd = interp(I1, I2)
+plot_rainmap(I1)
+plot_rainmap(interpd[0])
+plot_rainmap(I2)
 
-interp(I1, I2)
+t0 = ncdatetime(nc0)[0]
+t1 = ncdatetime(nc1)[0]
+dt = t1-t0
 
 #gdata = gio.read_radx_grid(datafilepath)
