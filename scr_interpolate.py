570c19add04cfe20d8ea12e7b1bd3e8cc131820c
Jussi Tiira <jussi.tiira@helsinki.fi>
2016-11-03 10:35:43 +0200

    interpolate all, png, etc

--- scr_interpolate.py
+++ scr_interpolate.py
@@ -20,6 +20,21 @@ import scipy.io
 #import pyart
 #import grid_io_withradx2gridread as gio
 
+plt.ioff()
+debug = False
+
+interval_s = 10.
+interval_dt = datetime.timedelta(seconds=interval_s)
+
+basepath = '/media/jussitii/04fafa8f-c3ca-48ee-ae7f-046cf576b1ee'
+gridpath = os.path.join(basepath, 'grid')
+resultspath = '/home/jussitii/results/radcomp'
+intrp_path = os.path.join(basepath, 'interpolated')
+
+SITES = ['KUM', 'KER', 'VAN']
+#MATPATH = os.path.join(intrp_path, 'mat')
+#PNGPATH = os.path.join(intrp_path, 'png')
+
 def interp(I1, I2, n=1):
     """Interpolate n frames."""
     VF,VB = motion(I1, I2)
@@ -37,21 +52,32 @@ def motion(I1, I2):
     return extract_motion_proesmans(Iu[0], Iu[1], lam=25.0, num_iter=250, num_levels=6)
 
 def plot_rainmap(r):
+    fig, ax = plt.subplots()
     r_ = r.copy()
     r_[r < 0.05] = np.nan
     plt.figure()
-    plt.imshow(r_, vmin=0.05, vmax=10)
-    cb = plt.colorbar()
+    cax = ax.imshow(r_, vmin=0.05, vmax=10)
+    cb = fig.colorbar(cax)
     cb.set_label("rain rate (mm/h)")
-    plt.xticks([])
-    plt.yticks([])
-    #plt.savefig("input1.png", bbox_inches="tight")
+    ax.set_xticks([])
+    ax.set_yticks([])
+    return fig, ax
+
+def db2lin(db):
+    return 10.**(db/10.)
 
 def nc_r(ncdata):
-    dbz = ncdata.variables['DBZ']
+    is_kerava_data = 'Kerava' in ncdata.title
+    if is_kerava_data:
+        dbz = ncdata.variables['DBZ_TOT']
+    else:
+        dbz = ncdata.variables['DBZ']
     dbzdata = dbz[0,0,:,:]
-    dbzdata_corrected = dbzdata+2
-    z=10.**(dbzdata_corrected/10.)
+    if is_kerava_data:
+        dbzdata_corrected = dbzdata+17
+    else:
+        dbzdata_corrected = dbzdata+2
+    z = db2lin(dbzdata_corrected)
     return 0.0292*z**(0.6536)
 
 def ensure_path(directory):
@@ -88,50 +114,74 @@ def filter_filepaths(filepaths_all):
     for filepath in filepaths_all:
         with nc.Dataset(filepath, 'r') as ncdata:
             if data_is_bad(ncdata):
-                print('bad: ' + filepath)
+                #print('bad: ' + filepath)
                 filepaths_good.remove(filepath)
     return filepaths_good
 
-def work(filepaths_all):
-    pass
+def batch_interpolate(filepaths_good, outpath, data_site=None, save_png = False):
+    #elev = []
+    #l_dt = []
+    #l_t0_str = []
+    #l_t1_str = []
+    for f0, f1 in itertools.izip(filepaths_good, filepaths_good[1:]):
+        if data_site is None:
+            for site in SITES:
+                if site in f0:
+                    data_site = site
+        nc0 = nc.Dataset(f0, 'r')
+        nc1 = nc.Dataset(f1, 'r')
+        #elev.append(nc0.variables['z0'][0])
+        I1 = nc_r(nc0)
+        I2 = nc_r(nc1)
+        t0 = ncdatetime(nc0)[0]
+        t1 = ncdatetime(nc1)[0]
+        dt = t1-t0
+        #l_t0_str.append(str(t0))
+        #l_t1_str.append(str(t1))
+        n = int(round(dt.total_seconds()/interval_s))
+        intrp_timestamps = [t0+i*interval_dt for i in range(1, n+1, 1)]
+        selection = [t.second<10 for t in intrp_timestamps]
+        #l_dt.append(dt)
+        intrp = np.array(interp(I1, I2, n))
+        for i in np.where(selection)[0]:
+            t = intrp_timestamps[i]
+            r = intrp[i]
+            datedir = t.strftime('%Y%m%d')
+            fbasename = t.strftime('intrp_%Y%m%d_%H%M%S')
+            matfname = fbasename + '.mat'
+            matsitepath = ensure_path(os.path.join(outpath, data_site, 'R', 'mat', datedir))
+            matfilepath = os.path.join(matsitepath, matfname)
+            mdict = {'time': np.array(str(t)), 'R': r}
+            scipy.io.savemat(matfilepath, mdict, do_compression=True)
+            if save_png:
+                pngfname = fbasename + '.png'
+                pngsitepath = ensure_path(os.path.join(outpath, data_site, 'R', 'png', datedir))
+                pngfilepath = os.path.join(pngsitepath, pngfname)
+                fig, ax = plot_rainmap(r)
+                ax.set_title(str(t))
+                fig.savefig(pngfilepath, bbox_inches="tight")
+                plt.close(fig)
 
-interval_s = 10.
-interval_dt = datetime.timedelta(seconds=interval_s)
 
-testpath = '/media/jussitii/04fafa8f-c3ca-48ee-ae7f-046cf576b1ee/test'
-testfilepaths = glob.glob(os.path.join(testpath, 'KER', '*.nc'))
-testfilepaths.sort()
-gridpath = '/media/jussitii/04fafa8f-c3ca-48ee-ae7f-046cf576b1ee/grid'
-
-d_filepaths_all = {}
-for site in ['KUM', 'KER', 'VAN']:
-    d_filepaths_all[site] = glob.glob(os.path.join(gridpath, site, '*', '*.nc'))
-
-d_filepaths_good = copy.deepcopy(d_filepaths_all)
-for site in d_filepaths_all:
-    pass
-
-testfilepaths_good = filter_filepaths(testfilepaths)
-elev = []
-l_dt = []
-l_t0_str = []
-l_t1_str = []
-interp_timestamps = []
-for f0, f1 in itertools.izip(testfilepaths_good, testfilepaths_good[1:]):
-    nc0 = nc.Dataset(f0, 'r')
-    nc1 = nc.Dataset(f1, 'r')
-    elev.append(nc0.variables['z0'][0])
-    I1 = nc_r(nc0)
-    I2 = nc_r(nc1)
-    t0 = ncdatetime(nc0)[0]
-    t1 = ncdatetime(nc1)[0]
-    dt = t1-t0
-    l_t0_str.append(str(t0))
-    l_t1_str.append(str(t1))
-    n = int(round(dt.total_seconds()/interval_s))
-    interp_timestamps.append([t0+i*interval_dt for i in range(1, n+1, 1)])
-    l_dt.append(dt)
-    #interpd = interp(I1, I2, n)
+if debug:
+    testpath = os.path.join(basepath, 'test')
+    testfilepaths = glob.glob(os.path.join(testpath, 'KER', '*.nc'))
+    testfilepaths.sort()
+    testfilepaths_good = filter_filepaths(testfilepaths)
+    test_intrp_path = os.path.join(testpath, 'interpolated')
+    batch_interpolate(testfilepaths_good, test_intrp_path, save_png=True)
+    kumfilepath = os.path.join(testpath, 'ncf_20160904_033827.nc')
+    kerfilepath = os.path.join(testpath, 'ncf_20160904_033918.nc')
+    vanfilepath = os.path.join(testpath, 'ncf_20160904_034033.nc')
+    kumnc = nc.Dataset(kumfilepath, 'r')
+    kernc = nc.Dataset(kerfilepath, 'r')
+    vannc = nc.Dataset(vanfilepath, 'r')
+else:
+    for site in SITES:
+        filepaths_all = glob.glob(os.path.join(gridpath, site, '*', '*.nc'))
+        filepaths_all.sort()
+        filepaths_good = filter_filepaths(filepaths_all)
+        batch_interpolate(filepaths_good, intrp_path, data_site=site, save_png=True)
 
 def testcase():
     #filename0 = 'ncf_20160904_033827.nc' # KUM
