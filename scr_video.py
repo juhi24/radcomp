d656c1b23977c12b9a39a555b6e97951550f13ff
Jussi Tiira <jussi.tiira@helsinki.fi>
2016-11-18 16:40:36 +0200

    adjusting

--- scr_video.py
+++ scr_video.py
@@ -13,6 +13,7 @@ import radx
 import copy
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 debug = False
 save_png = False
@@ -41,6 +42,7 @@ dbz_d = copy.deepcopy(sitencs_old)
 initialize = 10
 if not debug:
     for dt in times:
+        dt_nice = dt.strftime('%Y-%m-%d %H:%M')
         dtdir = dt.strftime('%Y%m%d')
         dtstr = dt.strftime('%Y%m%d_%H%M')
         matfname = 'intrp_' + dtstr + '.mat'
@@ -60,28 +62,22 @@ if not debug:
             initialize -= 1
             continue
         fig = plt.figure()
-        gs = gridspec.GridSpec(1, 2, width_ratios=(2.5, 1))
-        gs_r = gridspec.GridSpecFromSubplotSpec(3, 1, subplot_spec=gs[0], height_ratios=[25,1,1], hspace=0.01, wspace=0.01)
-        gs_dbz = gridspec.GridSpecFromSubplotSpec(3, 2, subplot_spec=gs[1], width_ratios=[10,1], hspace=0.01, wspace=0.01)
-        #gs = gridspec.GridSpec(4, 4, height_ratios=(5,5,5,1))
-        gs.update(left=0.0, wspace=0.0, hspace=0.0, right=.9)
-        #axr = plt.subplot(gs[:-1, :-1])
+        gs = gridspec.GridSpec(1, 2, width_ratios=(1.75, 1), left=0, wspace=0, hspace=0, right=.92, top=0.9, bottom=0.08)
+        gs_r = gridspec.GridSpecFromSubplotSpec(2, 1, subplot_spec=gs[0], height_ratios=[25,1], hspace=0.01, wspace=0.01)
+        gs_dbz = gridspec.GridSpecFromSubplotSpec(3, 2, subplot_spec=gs[1], width_ratios=[15,1], hspace=0.01, wspace=0.01)
         axr = plt.subplot(gs_r[:-1, :])
         axsite = {}
         for i, site in enumerate(radx.SITES):
-            #axsite[site] = plt.subplot(gs[i, -1])
             axsite[site] = plt.subplot(gs_dbz[i, 0])
-        #fig, axarr = plt.subplots(nrows=2, ncols=2)
-        #fig.suptitle(dt.strftime('%Y-%m-%d %H:%M'))
-        #axcb = plt.subplot(gs[-1, :-1])
         ax_cb_r = plt.subplot(gs_r[-1, :])
         ax_cb_dbz = plt.subplot(gs_dbz[:, -1])
         radx.plot_rainmap(r, fig=fig, ax=axr, cax=ax_cb_r, orientation='horizontal')
         for site in radx.SITES:
             ax = axsite[site]
             im = ax.imshow(dbz_d[site], vmin=-30, vmax=60)
-            ax.set_title(site)
+            ax.set_title(site, y=0.82, x=0.82)
             ax.set_xticks([])
             ax.set_yticks([])
-        cb_dbz = fig.colorbar(im, cax=ax_cb_dbz)
+        axr.set_title(dt_nice, y=0.93, x=0.8)
+        cb_dbz = fig.colorbar(im, cax=ax_cb_dbz, use_gridspec=False)
         cb_dbz.set_label('reflectivity (dBZ)')
