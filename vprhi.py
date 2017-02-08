e7cffef4cae6d32c897d65b809cded713ada30b7
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-02-08 13:50:35 +0200

    common median filtering

--- vprhi.py
+++ vprhi.py
@@ -8,7 +8,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.io
 import matplotlib as mpl
-import copy
 import collections
 from os import path
 from sklearn import decomposition
@@ -51,22 +50,23 @@ def vprhimat2pn(datapath):
 def plotpn(pn, fields=None, scaled=False, cmap='gist_ncar', **kws):
     if fields is None:
         fields = pn.items
-    vmins = {'ZH': -15, 'ZDR': -1, 'zdr': -1, 'RHO': 0, 'KDP': 0, 'kdp': 0, 'DP': 0, 'phidp': 0}
-    vmaxs = {'ZH': 30, 'ZDR': 4, 'zdr': 4, 'RHO': 1, 'KDP': 0.26, 'kdp': 0.26, 'DP': 360, 'phidp': 360}
-    labels = {'ZH': 'dBZ', 'ZDR': 'dB', 'zdr': 'dB', 'KDP': 'deg/km', 'kdp': 'deg/km', 'DP': 'deg'}
+    vmins = {'ZH': -15, 'ZDR': -1, 'RHO': 0, 'KDP': 0, 'DP': 0, 'PHIDP': 0}
+    vmaxs = {'ZH': 30, 'ZDR': 4, 'RHO': 1, 'KDP': 0.26, 'DP': 360, 'PHIDP': 360}
+    labels = {'ZH': 'dBZ', 'ZDR': 'dB', 'KDP': 'deg/km', 'DP': 'deg', 'PHIDP': 'deg'}
     fig, axarr = plt.subplots(len(fields), sharex=True, sharey=True)
     if not isinstance(axarr, collections.Iterable):
         axarr = [axarr]
     def m2km(m, pos):
         return '{:.0f}'.format(m*1e-3)
     for i, field in enumerate(fields):
+        fieldup = field.upper()
         ax = axarr[i]
         if scaled:
             scalekws = {'vmin': 0, 'vmax': 1}
             label = 'scaled'
-        elif field in labels:
-            scalekws = {'vmin': vmins[field], 'vmax': vmaxs[field]}
-            label = labels[field]
+        elif fieldup in labels:
+            scalekws = {'vmin': vmins[fieldup], 'vmax': vmaxs[fieldup]}
+            label = labels[fieldup]
         else:
             scalekws = {}
             label = field
@@ -85,20 +85,24 @@ def plotpn(pn, fields=None, scaled=False, cmap='gist_ncar', **kws):
     return fig, axarr
 
 def scale_data(pn):
-    scaling_limits = {'ZH': (-10, 30), 'ZDR': (0, 3), 'KDP': (0, 0.5), 
+    scaling_limits = {'ZH': (-10, 30), 'ZDR': (0, 3), 'zdr': (0, 3), 'KDP': (0, 0.5), 
                       'kdp': (0, 0.15)}
-    scaled = copy.deepcopy(pn)
+    scaled = pn.copy()
     for field, data in scaled.iteritems():
         data -= scaling_limits[field][0]
         data *= 1.0/scaling_limits[field][1]
         scaled[field] = data
     return scaled
 
-def fillna(pn):
-    nan_replacement = {'ZH': -10, 'ZDR': 0, 'zdr': 0, 'KDP': 0, 'kdp': 0}
-    for field in list(pn.items):
-        pn[field].fillna(nan_replacement[field], inplace=True)
-    return pn
+def fillna(dat, field=''):
+    data = dat.copy()
+    nan_replacement = {'ZH': -10, 'ZDR': 0, 'KDP': 0}
+    if isinstance(data, pd.Panel):
+        for field in list(data.items):
+            data[field].fillna(nan_replacement[field.upper()], inplace=True)
+    elif isinstance(data, pd.DataFrame):
+        data.fillna(nan_replacement[field.upper()], inplace=True)
+    return data
 
 def kdp2phidp(kdp, dr_km):
     kdp_filled = kdp.fillna(0)
@@ -107,18 +111,24 @@ def kdp2phidp(kdp, dr_km):
 def prepare_pn(pn, kdpmax=2, fltr_size=(20,2)):
     dr = pd.Series(pn.major_axis.values, index=pn.major_axis).diff().bfill()
     dr_km = dr/1000
-    pn_new = copy.deepcopy(pn)
+    pn_new = pn.copy()
     pn_new['KDP_orig'] = pn_new['KDP'].copy()
     pn_new['KDP'][pn_new['KDP']<0] = np.nan
     pn_new['phidp'] = kdp2phidp(pn_new['KDP'], dr_km)
-    nullmask = pn_new['KDP_orig'].isnull()
-    kdp = pn_new['KDP'].fillna(0)
+    kdp = pn_new['KDP'] # a view
     kdp[kdp>kdpmax] = 0
     kdp[kdp<0] = 0
-    kdp_filtered = median_filter(kdp, size=fltr_size)
-    kdp_filtered[nullmask] = np.nan
-    pn_new['kdp'] = kdp_filtered
-    return pn_new
+    return med_fltr(pn_new)
+
+def med_fltr(pn):
+    sizes = {'ZDR': (5, 1), 'KDP': (20, 2)}
+    nullmask = pn['ZH'].isnull()
+    new = fillna(pn[sizes.keys()])
+    for field, data in new.iteritems():
+        new[field] = median_filter(data, size=sizes[field])
+        new[field][nullmask] = np.nan
+    new.items=map(str.lower, new.items)
+    return pd.concat([new, pn])
 
 def prepare_data(pn, fields=['ZH', 'ZDR', 'kdp'], hmax=10e3, kdpmax=None):
     data = pn[fields, 0:hmax, :].transpose(0,2,1)
@@ -150,26 +160,16 @@ def reject_outliers(df, m=2):
     s = d.divide(mdev, axis=0).replace(np.inf, np.nan).fillna(0)
     return df[s<m].copy()
 
-def filter_test(sig, n, wn, **kws):
-    #fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, sharey=True)
-    fig, ax1 = plt.subplots()
-    ax2 = ax1
-    b, a = signal.butter(n, wn, **kws)
-    x = sig.index.values
-    y = signal.filtfilt(b, a, sig)
-    sig.plot(ax=ax1)
-    ax2.plot(x,y)
-
 def rolling_filter(df, window=5, stdlim=0.1, fill_value=0, **kws):
     r = df.rolling(window=window, center=True)
 
-dt0 = pd.datetime(2014, 2, 21, 15, 30)
+dt0 = pd.datetime(2014, 2, 21, 19, 30)
 dt1 = pd.datetime(2014, 2, 22, 15, 30)
 pn_raw = data_range(dt0, dt1)
 
 pn = prepare_pn(pn_raw)
-fields = ['ZH', 'ZDR', 'kdp']
-fig, axarr = plotpn(pn, fields=fields)
+fields = ['ZH', 'zdr', 'kdp']
+fig, axarr = plotpn(pn, fields=fields, cmap='viridis')
 
 plot_components = True
 hmax = 10000
@@ -192,8 +192,8 @@ classes = pd.Series(data=km.labels_, index=pn.minor_axis)
 for iax in [0,1]:
     class_colors(classes, ax=axarr[iax])
 
-data['zdr']=median_filter(data['ZDR'], size=(1,6))
-plotpn(data[['ZDR', 'zdr']].transpose(0,2,1), cmap='viridis')
+#data['zdr']=median_filter(data['ZDR'], size=(1,6))
+#plotpn(data[['ZDR', 'zdr']].transpose(0,2,1), cmap='viridis')
 
 
 #plot_classes(data_scaled, classes, n_eigens)
