988ae7141b12d17ad0ff6bd3ded57f989629129f
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-03-17 17:25:46 +0200

    scripting: finetune plots and storing

--- scripts/scr_case_study.py
+++ scripts/scr_case_study.py
@@ -21,11 +21,13 @@ TABLE_PKL = path.join(CACHE_DIR, 'ground_table.pkl')
 EVENTS_PKL = path.join(CACHE_DIR, 'ground_events.pkl')
 
 def store_ground_data(casesname_baecc='tiira2017_baecc', 
-                      casesname_1415='tiira2017_1415', **kws):
+                      casesname_1415='tiira2017_1415', cases=None, **kws):
     e = prepare.events(casesname_baecc=casesname_baecc, 
                        casesname_1415=casesname_1415)
-    table = prep_table(e, **kws)
+    table = prep_table(e, cases=cases, **kws)
     table.to_pickle(TABLE_PKL)
+    if cases is not None:
+        e = prep_events(e, cases)
     with open(EVENTS_PKL, 'wb') as f:
         pickle.dump(e, f)
     return table
@@ -48,12 +50,6 @@ def prep_table(e, cases=None, **kws):
 def plot_data(data, ax, **kws):
     return ax.plot(data.index, data.values, drawstyle='steps', **kws)
 
-def fltr_long_period(data_in, minutes=30):
-    data = data_in.copy()
-    selection = data.tdelta>pd.tseries.offsets.Minute(minutes).delta
-    data.loc[selection,['density','D_max','D_0_gamma', 'N_w']] = np.nan
-    return data
-
 def plot_case(name, cases, g, n_comp=20):
     scheme = '2014rhi_{n}comp'.format(n=n_comp)
     data_g = g.loc[name]
@@ -65,15 +61,19 @@ def plot_case(name, cases, g, n_comp=20):
     ax_d = axarr[-1]
     plot_data(data_g['intensity'], ax_r)
     ax_r.set_ylabel('LWE, mm$\,$h$^{-1}$')
+    ax_r.set_ylim([0, 6])
     plot_data(data_g['density'], ax_rho)
     ax_rho.set_ylabel('Density, kg$\,$m$^{-3}$')
+    ax_rho.set_ylim([0, 600])
     plot_data(data_g['D_max'], ax_d, label='$D_{max}$')
     plot_data(data_g['D_0_gamma'], ax_d, label='$D_0$')
     ax_d.set_ylabel('mm')
+    ax_d.set_ylim([0, 15])
     ax_d.legend()
-    #figs, axarrs = vpc.plot_classes(data_scaled, classes, pca.n_components)
+    return fig, axarr
 
 cases = case.read_cases('analysis')
 g = pd.read_pickle(TABLE_PKL)
 for name in cases.index.values:
-    plot_case(name, cases, g)
+    fig, axarr = plot_case(name, cases, g)
+    fig.savefig(path.join(results_dir, name + '.png'))
