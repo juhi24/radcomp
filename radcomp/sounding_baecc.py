bc7be4096303c24de1061553f8cc52e94ae255a9
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-03-31 15:03:45 +0300

    sounding choose index

--- radcomp/sounding_baecc.py
+++ radcomp/sounding_baecc.py
@@ -2,9 +2,15 @@
 import numpy as np
 import netCDF4 as nc
 import pandas as pd
+from os import path
+from j24 import home
 
 
-soundings_f = '/home/jussitii/DATA/arm/sounding/tmpsondewnpnM1.b1.20140121.125200..20140330.172000.custom.cdf'
+SOUNDING_DIR = path.join(home(), 'DATA', 'arm', 'sounding')
+all_soundings_f = 'tmpsondewnpnM1.b1.20140121.125200..20140330.172000.custom.cdf'
+sounding_f = 'tmpsondewnpnM1.b1.20140131.115000.cdf'
+all_soundings_path = path.join(SOUNDING_DIR, all_soundings_f)
+sounding_path = path.join(SOUNDING_DIR, sounding_f)
 #s = nc.Dataset(soundings_f)
 
 def time(ncdata, as_np=False):
@@ -14,14 +20,17 @@ def time(ncdata, as_np=False):
         return np.array(t0 + ncdata.variables['time_offset'][:]).astype('datetime64[s]')
     return pd.to_datetime(t0 + ncdata.variables['time_offset'][:], unit='s')
 
-def nc2df(ncdata, variables=None):
+def nc2df(ncdata, index='alt', variables=None):
     if variables is None:
         variables = list(ncdata.variables)
         for rmvar in ['base_time', 'time_offset', 'time']:
             variables.remove(rmvar)
-    df = pd.DataFrame(index=time(ncdata))
+    df = pd.DataFrame()
+    t = pd.Series(time(ncdata))
+    df['time'] = t
     for var in variables:
         df[var] = ncdata.variables[var]
+    df.index = df[index]
     return df
 
 def deltat(ncdata):
