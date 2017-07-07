eeddab07e6ae428f4d3d076205d50ff271da1452
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-07-07 19:05:02 +0300

    marker label

--- radcomp/qpe/radar.py
+++ radcomp/qpe/radar.py
@@ -7,9 +7,10 @@ import matplotlib.pyplot as plt
 
 
 class Radar:
-    def __init__(self, lat=None, lon=None):
+    def __init__(self, lat=None, lon=None, name=None):
         self.lat = lat
         self.lon = lon
+        self.name = name
 
     def radar_pixel(self, lats, lons):
         """radar pixel coordinates from latitude and longitude grids"""
@@ -21,13 +22,18 @@ class Radar:
         x = np.abs(lon1-self.lon).argmin()
         return x, y
 
-    def draw_marker(self, ax=None, marker='D', color='black', **kws):
+    def draw_marker(self, ax=None, marker='D', color='black', withname=False,
+                    bbox=None, **kws):
         if ax is None:
             ax = plt.gca()
-        return ax.plot(self.lon, self.lat, marker=marker, color=color, **kws)
+        marker = ax.plot(self.lon, self.lat, marker=marker, color=color, **kws)
+        if withname:
+            ax.text(self.lon+0.05, self.lat, self.name, bbox=bbox, **kws)
+        return marker
+        
 
-KER = Radar(lat=60.3881, lon=25.1139)
-KUM = Radar(lat=60.2045, lon=24.9633)
-VAN = Radar(lat=60.2706, lon=24.8690)
+KER = Radar(lat=60.3881, lon=25.1139, name='KER')
+KUM = Radar(lat=60.2045, lon=24.9633, name='KUM')
+VAN = Radar(lat=60.2706, lon=24.8690, name='VAN')
 RADARS = dict(KER=KER, KUM=KUM, VAN=VAN)
 
