8a4b77468da293b0468fc0ef85a1eff9ff394517
Jussi Tiira <jussi.tiira@helsinki.fi>
2017-08-10 15:57:18 +0300

    func: cloud top from z

--- radcomp/vertical/tools.py
+++ radcomp/vertical/tools.py
@@ -4,8 +4,15 @@
 Vertical profile classification
 @author: Jussi Tiira
 """
+import numpy as np
+
 
 def m2km(m, pos):
     '''formatting m in km'''
     return '{:.0f}'.format(m*1e-3)
 
+
+def cloud_top_h(z, zmin=0):
+    top = (z>zmin).loc[::-1].idxmax()
+    top[top==z.index[-1]] = np.nan
+    return top
