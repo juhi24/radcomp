1203547a63344daf50da31b0cde992e29ee261fa
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-01-03 16:56:31 +0200

    melting px detection v0.0.1

--- scripts/vertical/scr_melting_layer_detection.py
+++ scripts/vertical/scr_melting_layer_detection.py
@@ -5,9 +5,30 @@ __metaclass__ = type
 import matplotlib.pyplot as plt
 from radcomp.vertical import case
 
+
+def low_z(z):
+    return
+
+
+def low_rhohv(rhohv):
+    return rhohv < 0.95
+
+
+def melting_px_candidate(pn):
+    return low_rhohv(pn['RHO'])
+
+
 if __name__ == '__main__':
     plt.close('all')
     cases = case.read_cases('melting')
     c = cases.case.iloc[0]
-    c.plot(params=['ZH', 'zdr', 'kdp', 'RHO'], cmap='viridis')
+    c_snow = cases.case.iloc[1] # no melting
+    #c_snow.plot(params=['ZH', 'zdr', 'kdp', 'RHO'], cmap='viridis')
+    rhohv = c.data['RHO']
+    rhohv_snow = c_snow.data['RHO']
+    rho_sample = rhohv.iloc[:,20]
+    rho_snow_sample = rhohv_snow.iloc[:,20]
+    c.data['MLT'] = melting_px_candidate(c.data)
+    fig, axarr = c.plot(params=['ZH', 'zdr', 'kdp', 'RHO', 'MLT'], cmap='viridis')
+
 
