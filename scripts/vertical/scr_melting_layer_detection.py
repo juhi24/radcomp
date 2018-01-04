6b7b894b42837bc6e3d8a15ab034b7f568885566
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-01-04 16:48:03 +0200

    sequence length filter

--- scripts/vertical/scr_melting_layer_detection.py
+++ scripts/vertical/scr_melting_layer_detection.py
@@ -3,6 +3,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 __metaclass__ = type
 
 import matplotlib.pyplot as plt
+import peakutils
+from peakutils.plot import plot as pplot
 from radcomp.vertical import case
 
 
@@ -14,10 +16,17 @@ def rhohv_is_low(rhohv):
     return rhohv < 0.97
 
 
+def drop_short_bool_sequence(df_bool, limit=5):
+    """Drop sequences of true that are shorter than limit."""
+    return df_bool.rolling(limit, center=True).sum() > (limit-0.5)
+
+
 def melting_px_candidate(pn):
     low_rhohv = rhohv_is_low(pn['RHO'])
     low_dbz = dbz_is_low(pn['ZH'])
-    return low_rhohv & -low_dbz
+    melting = low_rhohv & -low_dbz
+    melting = drop_short_bool_sequence(melting)
+    return melting
 
 
 if __name__ == '__main__':
