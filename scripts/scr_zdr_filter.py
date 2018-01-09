cd0aa6f7dd25fa2e3da27d348b0479f7a5850504
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-01-09 17:06:49 +0200

    zdr filter testing script

---
+++ scripts/scr_zdr_filter.py
@@ -0,0 +1,14 @@
+# coding: utf-8
+"""Filter ZDR using rhohv."""
+from __future__ import absolute_import, division, print_function, unicode_literals
+__metaclass__ = type
+
+import matplotlib.pyplot as plt
+from radcomp.vertical import case
+
+if __name__ == '__main__':
+    plt.close('all')
+    cases = case.read_cases('zdr_clutter')
+    for i, c in cases.case.iteritems():
+        c.plot(params=['ZH', 'ZDR', 'zdr', 'RHO'])
+
