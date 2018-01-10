aa533ecaccae3a499914dea7c589fed5070c8f4d
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-01-10 17:35:02 +0200

    minor scripting

--- scripts/vertical/scr_zdr_filter.py
+++ scripts/vertical/scr_zdr_filter.py
@@ -4,11 +4,11 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 __metaclass__ = type
 
 import matplotlib.pyplot as plt
-from radcomp.vertical import case
+from radcomp.vertical import case, filtering
 
 if __name__ == '__main__':
     plt.close('all')
     cases = case.read_cases('zdr_clutter')
     for i, c in cases.case.iteritems():
-        c.plot(params=['ZH', 'ZDR', 'zdr', 'RHO'])
+        c.plot(params=['ZH', 'ZDR', 'zdr', 'RHO'], cmap='viridis')
 
