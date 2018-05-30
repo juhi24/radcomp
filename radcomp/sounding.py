857a85c6bd4168d2fe676d353f6bb7487a30dff3
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-05-30 19:10:59 +0300

    combine sounding codes

--- radcomp/sounding.py
+++ radcomp/sounding.py
@@ -1,20 +1,28 @@
 # coding: utf-8
 from __future__ import absolute_import, division, print_function, unicode_literals
 __metaclass__ = type
-"""
-@author: Jussi Tiira
-"""
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import timedelta
 
-def sounding_url(datetime):
-    year = datetime.year
-    mon = str(datetime.month).zfill(2)
-    day = str(datetime.day).zfill(2)
-    hour = str(datetime.hour).zfill(2)
-    urlformat = 'http://weather.uwyo.edu/cgi-bin/sounding?region=europe&TYPE=TEXT%3ALIST&YEAR={year}&MONTH={month}&FROM={day}{hour}&TO={day}{hour}&STNM=02963'
-    return urlformat.format(year=year, month=mon, day=day, hour=hour)
+
+def round_hours(timestamp, hres=12):
+    """round timestamp to hres hours"""
+    tt = timestamp + timedelta(hours=hres/2)
+    dt = timedelta(hours=tt.hour%hres, minutes=tt.minute, seconds=tt.second)
+    return tt - dt
+
+
+def sounding_url(t, dtype='text'):
+    out_type = dict(pdf='PDF%3ASTUVE', text='TEXT%3ASIMPLE')
+    baseurl = 'http://weather.uwyo.edu/cgi-bin/sounding'
+    query = '?region=europe&TYPE={type}&YEAR={year}&MONTH={month:02d}&FROM={day:02d}{hour:02d}&TO={day:02d}{hour:02d}&STNM=02963'
+    urlformat = baseurl + query
+    return urlformat.format(type=out_type[dtype], year=t.year, month=t.month,
+                            day=t.day, hour=t.hour)
+
 
 def read_sounding(datetime):
     skiprows = (0,1,2,3,4,5,6,8,9)
@@ -23,6 +31,7 @@ def read_sounding(datetime):
     data = data.drop(data.tail(1).index).astype(np.float)
     data.index = data.index.astype(np.float)
     return data
+
 
 def create_pn(freq='12H'):
     dt_start = pd.datetime(2015, 11, 1, 00)
@@ -38,9 +47,11 @@ def create_pn(freq='12H'):
     pn = pd.Panel(d).transpose(2,1,0)
     return pn
 
+
 def create_hdf(filepath='/home/jussitii/DATA/soundings.hdf', key='w1516'):
     with pd.HDFStore(filepath) as store:
         store[key] = create_pn()
+
 
 def reindex(data, force_asc=False):
     i=np.array(range(int(data.index.min()*10),int(data.index.max()*10+1)))/10.
@@ -49,10 +60,12 @@ def reindex(data, force_asc=False):
         return data.reindex(i)
     return data.reindex(ii)
 
+
 def interp_akima(data):
     data = reindex(data, force_asc=True)
     intrp = data.interpolate(method='akima')
     return intrp.sort_index(ascending=False)
+
 
 def comp_interp_methods(rh):
     methods = ('linear', 'index', 'values', 'nearest', 'zero','slinear', 'quadratic', 'cubic', 'barycentric', 'piecewise_polynomial', 'from_derivatives', 'pchip', 'akima')
--- scripts/vertical/scr_open_wyoming_soundings.py
+++ scripts/vertical/scr_open_wyoming_soundings.py
@@ -7,7 +7,7 @@ import webbrowser
 import time
 import pandas as pd
 from datetime import timedelta
-from radcomp.vertical import case, classification
+from radcomp.vertical import multicase, classification, sounding
 
 case_set = 'melting'
 n_eigens = 19
@@ -24,26 +24,16 @@ name = classification.scheme_name(basename='14-16', n_eigens=n_eigens,
                                   radar_weight_factors=radar_weight_factors)
 name = 'mlt_18eig17clus_pca'
 
-
-def round_hours(tm, hres=12):
-     tt = tm + timedelta(hours=hres/2)
-     return tt - timedelta(hours=tt.hour%hres, minutes=tt.minute,
-                              seconds=tt.second)
-
 if __name__ == '__main__':
-    search_class = 9
-    cases = case.read_cases(case_set)
-    c = case.Case.by_combining(cases)
+    search_class = 11
+    c = multicase.MultiCase.from_caselist(case_set, filter_flag='ml_ok')
     c.load_classification(name)
-    baseurl = 'http://weather.uwyo.edu/cgi-bin/sounding'
-    query = '?region=europe&TYPE=PDF%3ASTUVE&YEAR={year}&MONTH={month:02d}&FROM={day:02d}{hour:02d}&TO={day:02d}{hour:02d}&STNM=02963'
-    urlformat = baseurl+query
     matchi = c.classes[c.classes==search_class].index
     matcha = pd.Series(data=matchi, index=matchi)
     match = pd.concat([matcha.iloc[0:1], matcha[matcha.diff()>timedelta(hours=2)]])
     for d, cla in match.iteritems():
-        t = round_hours(d, hres=12)
-        url = urlformat.format(year=t.year, month=t.month, day=t.day, hour=t.hour)
+        t = sounding.round_hours(d, hres=12)
+        url = sounding.sounding_url(t, dtype='text')
         webbrowser.open(url)
         time.sleep(1)
 
