c3e86fe368b5174cb872561beabc16e555fb0b40
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-06-28 10:53:27 +0300

    snow training script up to date with API

--- scripts/vertical/scr_train.py
+++ scripts/vertical/scr_train.py
@@ -5,44 +5,45 @@ A script for using the VP classification training method
 import numpy as np
 import matplotlib.pyplot as plt
 from os import path
-from radcomp import RESULTS_DIR
-from radcomp.vertical import case, classification
+from radcomp.vertical import multicase, classification, RESULTS_DIR
+from j24 import ensure_join
+
 
 plt.ion()
 plt.close('all')
 np.random.seed(1)
 
-plot = False
+savefig = False
 
-cases = case.read_cases('melting-confirmed')
-basename = 'mlt'
+cases = multicase.read_cases('14-16by_hand')
+basename = '14-16'
 params = ['ZH', 'zdr', 'kdp']
 hlimits = (190, 10e3)
-n_eigens = 10
-n_clusters = 10
+n_eigens = 19
+n_clusters = 19
 reduced = True
-use_temperature = False
+use_temperature = True
 t_weight_factor = 0.8
-radar_weight_factors = dict()
-
-if plot:
-    for name, c in cases.case.iteritems():
-        fig, axarr = case.plot(params=params, cmap='viridis')
-        savepath = path.join(RESULTS_DIR, 'cases', name+'.png')
-        fig.savefig(savepath)
+radar_weight_factors = dict(zdr=0.5)
 
 scheme = classification.VPC(params=params, hlimits=hlimits, n_eigens=n_eigens,
                             reduced=reduced, t_weight_factor=t_weight_factor,
                             radar_weight_factors=radar_weight_factors,
-                            basename=basename, n_clusters=n_clusters)
-c = case.Case.by_combining(cases, class_scheme=scheme)
-c.train()
+                            basename=basename, n_clusters=n_clusters,
+                            use_temperature=use_temperature)
+c = multicase.MultiCase.by_combining(cases, class_scheme=scheme)
+trainkws = {}
+#if reduced:
+    #trainkws['n_clusters'] = n_clusters
+    #trainkws['use_temperature'] = use_temperature
+c.train(**trainkws)
 scheme.save()
 # Load classification and plot centroids
 name = c.class_scheme.name()
 print(name)
 c.load_classification(name)
-order = c.clus_centroids()[0].ZH.iloc[0]
-c.plot_cluster_centroids(cmap='viridis', colorful_bars='blue',
-                         sortby=order)
-
+fig, axarr, i = c.plot_cluster_centroids(colorful_bars='blue')
+if savefig:
+    savedir = ensure_join(RESULTS_DIR, 'classes_summary', name)
+    savefile = path.join(savedir, 'centroids.png')
+    fig.savefig(savefile, bbox_inches='tight')
