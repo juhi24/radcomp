d0b2263db8e4ae846355f023d723ae60ad5bca78
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-06-28 10:54:58 +0300

    rhi plotting script

---
+++ scripts/vertical/scr_plot_rhi.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+__metaclass__ = type
+
+import pyart
+import matplotlib.pyplot as plt
+from os import path
+from radcomp.vertical import RESULTS_DIR
+from j24 import home
+
+
+if __name__ == '__main__':
+    plt.close('all')
+    rawfile = path.join(home(), 'DATA', '201402212355_IKA.RHI_HV.raw')
+    outfile = path.join(RESULTS_DIR, 'rhi_sample.png')
+    radar = pyart.io.read(rawfile)
+    fig, ax = plt.subplots()
+    display = pyart.graph.RadarDisplay(radar)
+    display.plot('reflectivity')
+    ax.set_ylim(bottom=0, top=10)
+    ax.set_xlim(left=0, right=80)
+    fig.savefig(outfile)
