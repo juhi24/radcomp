8b708c5d9defd5c685f8682b7a703e43b3196baf
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-08-08 16:08:29 +0300

    plot silh scores

--- scripts/vertical/conf.py
+++ scripts/vertical/conf.py
@@ -3,8 +3,14 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 __metaclass__ = type
 
+from os import path
+from radcomp.vertical import RESULTS_DIR
+
+
 SCHEME_ID_SNOW = '14-16_t08_zdr05_19eig19clus_pca'
 SCHEME_ID_MELT = 'mlt_18eig17clus_pca'
 
 CASES_SNOW = '14-16by_hand'
 CASES_MELT = 'melting'
+
+P1_FIG_DIR = path.join(RESULTS_DIR, 'paper1')
--- scripts/vertical/scr_class_metrics.py
+++ scripts/vertical/scr_class_metrics.py
@@ -5,17 +5,18 @@ __metaclass__ = type
 import pandas as pd
 import matplotlib.pyplot as plt
 from radcomp.vertical import multicase, classification
-from conf import SCHEME_ID_MELT, CASES_MELT
+from j24.tools import notify
+from conf import SCHEME_ID_MELT, CASES_MELT, P1_FIG_DIR
 
 
 case_set = CASES_MELT
 scheme_id = SCHEME_ID_MELT
 
 
-basename = 'mlt'
+basename = 'mlt2'
 params = ['ZH', 'zdr', 'kdp']
 hlimits = (190, 10e3)
-n_eigens = 15
+n_eigens = 18
 reduced = True
 use_temperature = False
 radar_weight_factors = dict()
@@ -24,7 +25,7 @@ radar_weight_factors = dict()
 def silh_score_avgs(cc, n_iter=10, **kws):
     scores = pd.DataFrame()
     for i in range(n_iter):
-        for n_classes in range(5, 25):
+        for n_classes in range(8, 25):
             scheme = classification.VPC(params=params, hlimits=hlimits,
                                         n_eigens=n_eigens, reduced=reduced,
                                         radar_weight_factors=radar_weight_factors,
@@ -38,18 +39,38 @@ def silh_score_avgs(cc, n_iter=10, **kws):
     return scores
 
 
+def plot_scores(scores, ax=None, **kws):
+    n_classes = scores.columns.values
+    ax = ax or plt.gca()
+    score = scores.mean()
+    std = scores.std()
+    score.plot(ax=ax, color='black', label='mean score')
+    ax.fill_between(n_classes, score-std, score+std, alpha=0.3,
+                    facecolor='blue', label='score std', **kws)
+    ax.set_xlabel('Number of classes')
+    ax.set_ylabel('Silhouette score')
+    ax.set_xlim(left=n_classes[0], right=n_classes[-1])
+    ax.legend()
+    ax.grid(axis='x')
+    return ax
+
+
 if __name__ == '__main__':
     plt.close('all')
     cases = multicase.read_cases(case_set)
     cases = cases[cases.ml_ok.astype(bool)]
     cc = multicase.MultiCase.by_combining(cases, has_ml=True)
-    cc.load_classification(scheme_id)
-    #scores = silh_score_avgs(cc, n_iter=5, n_pc=4)
+    #cc.load_classification(scheme_id)
+    scores = silh_score_avgs(cc, n_iter=10, n_pc=3)
+    notify('Silhouette score', 'Score calculation finished.')
+    fig, ax = plt.subplots(dpi=110, figsize=(5,4))
+    plot_scores(scores, ax=ax)
+    fig.savefig()
     #scores.mean().plot()
     #cases.case.apply(lambda x: x.load_classification(scheme_id))
     #c = cases.case.iloc[0]
-    order = cc.clus_centroids()[0].ZH.iloc[0]
-    cc.plot_cluster_centroids(cmap='viridis', colorful_bars='blue', sortby=order)
+    #order = cc.clus_centroids()[0].ZH.iloc[0]
+    #cc.plot_cluster_centroids(cmap='viridis', colorful_bars='blue', sortby=order)
     #fig, ax = plt.subplots()
     #cc.plot_silhouette(ax=ax)
 
