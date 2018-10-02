cb54a3f1dd9492a2174685b8d99505d0030b611a
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-10-02 14:57:12 +0300

    script to play with IKA rhi

---
+++ scripts/vertical/scr_ika_raw_sandbox.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+from __future__ import absolute_import, division, print_function, unicode_literals
+__metaclass__ = type
+
+import pyart.io
+import numpy as np
+import pandas as pd
+from os import path
+from glob import glob
+from radcomp.vertical import RESULTS_DIR
+
+
+def lonlat2csv(lon, lat, path_or_buf=None):
+    """Save arrays of longitude and latitude to csv."""
+    lonlat = pd.DataFrame(np.array([lon, lat]).T)
+    path_or_buf = path_or_buf or path.join(RESULTS_DIR, 'ika_rhi_latlon.csv')
+    lonlat.to_csv(path_or_buf, index=False, header=False)
+
+
+if __name__ == '__main__':
+    lon_target = 24.28878
+    datadir = path.expanduser('~/DATA/IKA/20180818RHI')
+    filefmt = '*RHI*.raw'
+    filesglob = path.join(datadir, filefmt)
+    files = glob(filesglob)
+    radar = pyart.io.read(files[0])
+    z = radar.fields['reflectivity']['data']
+    low_lat = radar.gate_latitude['data'][0]
+    low_alt = radar.gate_altitude['data'][0]
+    low_lon = radar.gate_longitude['data'][0]
+
+
