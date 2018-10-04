4be45afd252697c3561d9ed4020bd1df9e039235
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-10-04 13:01:12 +0300

    maesaka kdp, aggregation workflow, accurate r_hyde

--- scripts/vertical/scr_rhi2vp.py
+++ scripts/vertical/scr_rhi2vp.py
@@ -8,11 +8,30 @@ __metaclass__ = type
 
 import pyart
 import numpy as np
-import copy
-import glob
 import scipy.io as sio
+from glob import glob
 from os import path
 from datetime import datetime
+
+
+R_IKA_HYDE = 64450 # m
+
+
+def lin_agg(db, agg_fun=np.nanmean, **kws):
+    """aggregate in linear space"""
+    lin = db2lin(db)
+    aggregated = agg_fun(lin, **kws)
+    return lin2db(aggregated)
+
+
+def db2lin(db):
+    """decibels to linear scale"""
+    return np.power(10, db/10)
+
+
+def lin2db(lin):
+    """linear to decibel scale"""
+    return 10*np.log10(lin)
 
 
 def calibration(radar, field_name, addition):
@@ -22,10 +41,15 @@ def calibration(radar, field_name, addition):
     radar.fields.update({field_name: field})
 
 
+def interp(h_target, h_orig, var, agg_fun=np.nanmedian):
+    var_agg = agg_fun(var, axis=1)
+    return np.interp(h_target, h_orig, var_agg)
+
+
 def main(pathIn, pathOut, hmax=15000):
     """Extract profiles and save as mat."""
     file_indx = 0
-    files = np.sort(glob.glob(path.join(pathIn, "*RHI_HV*.raw")))
+    files = np.sort(glob(path.join(pathIn, "*RHI_HV*.raw")))
     nfile = len(files)
     n_hbins = 301
     zh_vp    = np.zeros([nfile, n_hbins])
@@ -37,6 +61,7 @@ def main(pathIn, pathOut, hmax=15000):
     height   = np.linspace(0, hmax, n_hbins)
     for filename in files:
         try: ## reading radar data
+            print(filename)
             radar = pyart.io.read(filename)
 
             ### Fixing calibration of ZDR ad ZH
@@ -52,63 +77,61 @@ def main(pathIn, pathOut, hmax=15000):
         except:
             print("print could not read data")
             continue
-        try: ## creating display object
-            display = pyart.graph.RadarDisplay(radar)
-
+        try:
             # Slant range
             #r = np.sqrt(np.power(display.x,2) + np.power(display.y,2))
             r = radar.gate_x['data']
-            r_hyde = 64000
-            margin = 5e3
+            r_hyde = R_IKA_HYDE
+            margin = 1e3
             rmin = r_hyde - margin
             rmax = r_hyde + margin
 
             # Extracting radar variables
-            ZH  = copy.deepcopy(display.fields['reflectivity'])
-            ZH  = np.power(10.0,ZH['data']/10.0)
-            ZDR = copy.deepcopy(display.fields['differential_reflectivity'])
-            ZDR = np.power(10.0,ZDR['data']/10.0)
-            KDP = copy.deepcopy(display.fields['specific_differential_phase'])
-            KDP = KDP['data']
-            RHO = copy.deepcopy(display.fields['cross_correlation_ratio'])
-            RHO = RHO['data']
-            DP  = copy.deepcopy(display.fields['differential_phase'])
-            DP  = DP['data']
-
-            ZH[r<=rmin] = np.NaN
-            ZH[r>=rmax]  = np.NaN
-
-            ZDR[r<=rmin] = np.NaN
-            ZDR[r>=rmax]  = np.NaN
-
-            KDP[r<=rmin] = np.NaN
-            KDP[r>=rmax]  = np.NaN
-
-            RHO[r<=rmin] = np.NaN
-            RHO[r>=rmax]  = np.NaN
+            ZH  = radar.fields['reflectivity'].copy()['data']
+            ZDR = radar.fields['differential_reflectivity'].copy()['data']
+            #KDP = radar.fields['specific_differential_phase'].copy()['data']
+            RHO = radar.fields['cross_correlation_ratio'].copy()['data']
+            DP = radar.fields['differential_phase'].copy()['data']
+            kdp_m = pyart.retrieve.kdp_maesaka(radar)
+            KDP = np.ma.masked_array(data=kdp_m[0]['data'], mask=ZH.mask)
+            for var in [ZH, ZDR, KDP, RHO]:
+                var.set_fill_value(np.nan)
+                var.mask[r<=rmin] = True
+                var.mask[r>=rmax] = True
+            ZH = ZH.filled()
+            ZDR = ZDR.filled()
+            KDP = KDP.filled()
+            RHO = RHO.filled()
+            DP = DP.filled()
 
             diff_r = np.abs(r[0,:] - r_hyde)
             indx = diff_r.argmin()
-
             #hght = display.z[:,indx]
-            hght = radar.gate_z['data'][:,indx]
+            hght = radar.gate_z['data'][:, indx]
+            #dr = np.abs(r-r_hyde)
+            #ix = dr.argmin(axis=1)
+            #hght = radar.gate_z['data'][range(ix.size),ix]
 
-            zh_vp[file_indx,:]  = np.interp(height, hght, 10*np.log10(np.nanmean(ZH,1)))
-            zdr_vp[file_indx,:] = np.interp(height, hght, 10*np.log10(np.nanmean(ZDR,1)))
-            kdp_vp[file_indx,:] = np.interp(height, hght, np.nanmean(KDP,1))
-            rho_vp[file_indx,:] = np.interp(height, hght, np.nanmean(RHO,1))
-            dp_vp[file_indx,:]  = np.interp(height, hght, np.nanmean(DP,1))
+            agg_fun = np.nanmedian
+            def agg_fun_db(x, **kws):
+                if agg_fun == np.nanmedian:
+                    return agg_fun(x, **kws)
+                return lin_agg(x, agg_fun=agg_fun, **kws)
+            zh_vp[file_indx,:]  = interp(height, hght, ZH, agg_fun_db)
+            zdr_vp[file_indx,:] = interp(height, hght, ZDR, agg_fun_db)
+            kdp_vp[file_indx,:] = interp(height, hght, KDP, agg_fun)
+            rho_vp[file_indx,:] = interp(height, hght, RHO, agg_fun)
+            dp_vp[file_indx,:]  = interp(height, hght, DP, agg_fun)
 
-            #ObsTime.append(display.time_begin.isoformat())
             tstr = path.basename(filename)[0:12]
             ObsTime.append(datetime.strptime(tstr, '%Y%m%d%H%M').isoformat())
-            file_indx = file_indx + 1
+            file_indx += 1
         except Exception as error:
             print(str(error))
             print("print could not plot data")
             raise error
             continue
-    fname_supl = 'IKA_VP_from_RHI'
+    fname_supl = 'IKA_vprhi_1km_median_maesaka'
     time_filename = path.basename(filename)[0:8]
     fileOut = path.join(pathOut, time_filename + '_' + fname_supl + '.mat')
     print(fileOut)
