24174edcd3bef78f6295823ea416f89303939a66
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-11-14 16:09:15 +0200

    combine training scripts

--- scripts/vertical/scr_train.py
+++ scripts/vertical/scr_train.py
@@ -7,36 +7,42 @@ import matplotlib.pyplot as plt
 from os import path
 from radcomp.vertical import multicase, classification, RESULTS_DIR
 from j24 import ensure_join
-from conf import VPC_PARAMS_SNOW
+from conf import VPC_PARAMS_SNOW, VPC_PARAMS_RAIN
 
 
 save_plots = True
 train = True
 save_scheme = True
 
-VPC_PARAMS_SNOW.update({'n_clusters': 19,
-                        'radar_weights': {'kdp': 1.7},
-                        'n_eigens': 30})
+VPC_PARAMS_SNOW.update({'n_clusters': 14})
+VPC_PARAMS_RAIN.update({'n_clusters': 11})
 
 
 if __name__ == '__main__':
     plt.ion()
     plt.close('all')
     np.random.seed(1)
-    cases = multicase.read_cases('snow')
-    scheme = classification.VPC(**VPC_PARAMS_SNOW)
-    c = multicase.MultiCase.by_combining(cases, class_scheme=scheme)
+    cases_id = 'snow'
+    rain_season = cases_id in ('rain',)
+    cases = multicase.read_cases(cases_id)
+    if rain_season:
+        cases = cases[cases.ml_ok.fillna(0).astype(bool)]
+    vpc_params = VPC_PARAMS_RAIN if rain_season else VPC_PARAMS_SNOW
+    scheme = classification.VPC(**vpc_params)
+    c = multicase.MultiCase.by_combining(cases, class_scheme=scheme,
+                                         has_ml=rain_season)
     if train:
-        c.train()
+        c.train(quiet=False)
         if save_scheme:
             scheme.save()
     # Load classification and plot centroids
     c.load_classification()
-    fig, axarr, i = c.plot_cluster_centroids(colorful_bars='blue')
+    fig, axarr, i = c.plot_cluster_centroids(colorful_bars='blue',
+                                             cmap='viridis')
     ax_sca = c.scatter_class_pca(plot3d=True)
     fig_s, ax_s = plt.subplots()
-    c.plot_silhouette(ax=ax_s, cols=(0, 1, 2))#, 'temp_mean'))
+    c.plot_silhouette(ax=ax_s)
     if save_plots:
         savedir = ensure_join(RESULTS_DIR, 'classes_summary', c.class_scheme.name())
-        savefile = path.join(savedir, 'centroids.png')
-        fig.savefig(savefile, bbox_inches='tight')
+        fig.savefig(path.join(savedir, 'centroids.png'), bbox_inches='tight')
+        fig_s.savefig(path.join(savedir, 'silhouettes.png'), bbox_inches='tight')
