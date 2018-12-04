707432ff3465c54b56e071504325ac59fd93cd4f
Jussi Tiira <jussi.tiira@helsinki.fi>
2018-12-04 13:24:50 +0200

    setting active case, docstrings

--- radcomp/vertical/cases_plotter.py
+++ radcomp/vertical/cases_plotter.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 
 
 class CasesPlotter:
+    """Interactively visualize a DataFrame of cases"""
+
     def __init__(self, cases):
         self.cases = cases
         self.fig = None
@@ -16,30 +18,39 @@ class CasesPlotter:
 
     @property
     def n_cases(self):
+        """number of cases"""
         return self.cases.shape[0]
 
     def next_case(self):
+        """Set next case as active."""
         if self.i_active > self.n_cases-2:
             return self.i_active
         self.i_active += 1
         return self.i_active
 
     def prev_case(self):
+        """Set previous case as active."""
         if self.i_active < 1:
             return 0
         self.i_active -= 1
         return self.i_active
 
     def active_case(self, i=None):
-        i = i or self.i_active
+        """Get and optionally set active case."""
+        if i is None:
+            i = self.i_active
+        else:
+            self.i_active = i
         return self.cases.case.iloc[i]
 
     def plot(self, **kws):
+        """Plot current active case."""
         self.plot_kws = kws
         self.fig, self.axarr = self.active_case().plot(**kws)
-        self.fig.canvas.mpl_connect('key_press_event', self.press)
+        self.fig.canvas.mpl_connect('key_press_event', self._press)
 
-    def press(self, event):
+    def _press(self, event):
+        """key press logic"""
         print(event.key)
         if event.key == 'right':
             self.next_case()
