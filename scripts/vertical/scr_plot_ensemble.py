26a2cabd70f4bad6f13dcf582023ff1266dcf16b
Jussi Tiira <jussi.tiira@helsinki.fi>
2019-01-22 10:49:38 +0200

    t boxplot

--- scripts/vertical/scr_plot_ensemble.py
+++ scripts/vertical/scr_plot_ensemble.py
@@ -10,6 +10,16 @@ from radcomp.vertical import multicase, plotting, case
 import conf
 
 
+def t_boxplot(c):
+    t_cl = pd.concat([c.t_surface(), c.classes], axis=1)
+    ax = t_cl.boxplot(by='class')
+    ax.set_title('Surface temperature distribution by class')
+    ax.set_xlabel('class')
+    ax.set_ylabel('$T_s$')
+    ax.get_figure().suptitle('')
+    return ax
+
+
 if __name__ == '__main__':
     plt.ion()
     plt.close('all')
@@ -19,7 +29,7 @@ if __name__ == '__main__':
     #c = multicase.MultiCase.from_caselist(cases_id, filter_flag=flag, has_ml=rain_season)
     name = conf.SCHEME_ID_RAIN if rain_season else conf.SCHEME_ID_SNOW
     #c.load_classification(name)
-    for cl in [7]:# c.class_scheme.get_class_list():
+    for cl in c.class_scheme.get_class_list():
         dat = case.fillna(c.data.loc[('zh', 'kdp', 'zdr'), :10000, c.classes==cl])
         axarr = plotting.plot_vps(dat.iloc[:,:,0], linewidth=0.5, alpha=0)
         for dt in dat.minor_axis:
@@ -34,3 +44,4 @@ if __name__ == '__main__':
         plotting.plot_vps_betweenx(q1, q3, axarr=axarr, alpha=0.5)
         fig = axarr[0].get_figure()
         fig.suptitle('Class {}'.format(cl))
+    t_boxplot(c)
