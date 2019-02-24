6fff71297e855b70130ab5b2b182fbf45619934a
Jussi Tiira <jussi.tiira@helsinki.fi>
2019-02-24 19:11:05 +0200

    SCR: find cases to combine

--- scripts/vertical/scr_split_cases_dev.py
+++ scripts/vertical/scr_split_cases_dev.py
@@ -7,11 +7,22 @@ import pandas as pd
 
 if __name__ == '__main__':
     diffs = cc.timestamps().diff()
-    casegaps = diffs[diffs>cc.timedelta]
+    grouper_orig = (diffs>cc.timedelta).cumsum()
+    grouper_orig.name = 'g_orig'
+    orig_gaps = diffs[diffs>cc.timedelta]
     gap = datetime.timedelta(hours=12)
-    echo_gaps = diffs.copy()
+    ts = cc.timestamps()
     no_echo = cc.classes()==0
     i_no_echo = no_echo[no_echo].index
-    echo_gaps.drop(i_no_echo, inplace=True)
+    ts.drop(i_no_echo, inplace=True)
+    echo_gaps = ts.diff()
+    echo_gaps.name = 'echo_gap'
 
+    b=pd.concat([echo_gaps, grouper_orig], axis=1).dropna()
+    c=b.shift()
 
+    grouper_new = (echo_gaps>gap).cumsum()+1000
+    groupers = pd.concat([grouper_new, grouper_orig], axis=1)
+    g_drop=groupers.dropna()
+    g=g_drop.g_orig.groupby(g_drop.echo_gap)
+    combinations = g.unique()
