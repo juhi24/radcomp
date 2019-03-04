64399fd8e67e00c131ee781accdc95f1878d555b
Jussi Tiira <jussi.tiira@helsinki.fi>
2019-03-04 14:46:00 +0200

    complete case combiner dev

--- scripts/vertical/scr_split_cases_dev.py
+++ scripts/vertical/scr_split_cases_dev.py
@@ -1,28 +1,48 @@
 # coding: utf-8
+"""Rearrange cases based on radar echos."""
 
 import datetime
 
 import pandas as pd
 
+from radcomp.vertical import multicase
 
-if __name__ == '__main__':
+
+def _icombine(comb, cases):
+    ccases = cases.iloc[comb]
+    return multicase.MultiCase.by_combining(ccases)
+
+
+def grouper_orig(cc):
+    """grouper based on gaps in timestamps"""
     diffs = cc.timestamps().diff()
-    grouper_orig = (diffs>cc.timedelta).cumsum()
-    grouper_orig.name = 'g_orig'
-    orig_gaps = diffs[diffs>cc.timedelta]
-    gap = datetime.timedelta(hours=12)
+    grpr_orig = (diffs>cc.timedelta).cumsum()
+    grpr_orig.name = 'g_orig'
+    return grpr_orig
+
+
+def echo_gaps(cc):
+    """Series of timedeltas since previous echo"""
     ts = cc.timestamps()
     no_echo = cc.classes()==0
     i_no_echo = no_echo[no_echo].index
     ts.drop(i_no_echo, inplace=True)
-    echo_gaps = ts.diff()
-    echo_gaps.name = 'echo_gap'
+    gaps = ts.diff()
+    gaps.name = 'echo_gap'
+    return gaps
 
-    b=pd.concat([echo_gaps, grouper_orig], axis=1).dropna()
-    c=b.shift()
 
-    grouper_new = (echo_gaps>gap).cumsum()+1000
-    groupers = pd.concat([grouper_new, grouper_orig], axis=1)
+def combine_cases_t_thresh(cases, gap=datetime.timedelta(hours=12)):
+    """Combine cases with echo gaps less than threshold."""
+    cc = multicase.MultiCase.by_combining(cases)
+    grpr_orig = grouper_orig(cc)
+    egaps = echo_gaps(cc)
+    grouper_new = (egaps>gap).cumsum()+10000
+    groupers = pd.concat([grouper_new, grpr_orig], axis=1)
     g_drop=groupers.dropna()
-    g=g_drop.g_orig.groupby(g_drop.echo_gap)
+    g = g_drop.g_orig.groupby(g_drop.echo_gap)
     combinations = g.unique()
+    cases_new = combinations.apply(_icombine, args=[cases])
+    cases_new.index=cases_new.apply(lambda x: x.name())
+    cases_new.name = 'case'
+    return cases_new
