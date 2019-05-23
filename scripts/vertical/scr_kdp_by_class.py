01b73adfd55fc84b122e1f3ffda420e7fdce4f05
Jussi Tiira <jussi.tiira@helsinki.fi>
2019-05-23 19:28:32 +0300

    SCR: more kdp vs t_s plotting

--- scripts/vertical/scr_kdp_by_class.py
+++ scripts/vertical/scr_kdp_by_class.py
@@ -7,18 +7,42 @@ from radcomp.visualization import LABELS
 from j24.visualization import scatter_kde
 
 
-if __name__ == '__main__':
-    plt.ion()
-    t12 = cc.data.T.loc[:, cc.classes()==12]
-    kdp12 = cc.data.kdp.loc[:, cc.classes()==12]
-    kdpg12 = cc.data.kdpg.loc[:, cc.classes()==12]
-    selection = ~np.isnan(kdpg12.values.flatten())
-    x = kdp12.values.flatten()[selection]
-    y = t12.values.flatten()[selection]
+def scatter_kdp(kdp, t):
     fig, ax = plt.subplots()
-    scatter_kde(x, y, ax=ax)
+    selection = ~np.isnan(kdp.values.flatten())
+    kdp_flat = kdp.values.flatten()[selection]
+    t_flat = t.values.flatten()[selection]
+    scatter_kde(kdp_flat, t_flat, ax=ax)
     ax.set_xlim(left=0, right=0.2)
     ax.set_ylim(bottom=-35, top=0)
     ax.invert_yaxis()
     ax.set_ylabel(LABELS['T'])
     ax.set_xlabel(LABELS['KDP'])
+    return ax
+
+
+def scatter_kdpg(kdpg, t):
+    fig, ax = plt.subplots()
+    selection = ~np.isnan(kdpg.values.flatten())
+    kdpg_flat = kdpg.values.flatten()[selection]
+    t_flat = t.values.flatten()[selection]
+    scatter_kde(kdpg_flat, t_flat, ax=ax)
+    ax.set_xlim(left=-2, right=6)
+    ax.set_ylim(bottom=-35, top=0)
+    ax.invert_yaxis()
+    ax.set_ylabel(LABELS['T'])
+    ax.set_xlabel(LABELS['KDPG'])
+    return ax
+
+
+if __name__ == '__main__':
+    plt.ion()
+    for cl in range(cc.vpc.n_clusters):
+        t = cc.data['T'].loc[:, cc.classes()==cl]
+        kdp = cc.data.kdp.loc[:, cc.classes()==cl]
+        kdpg = cc.data.kdpg.loc[:, cc.classes()==cl]
+        ax = scatter_kdp(kdp, t)
+        title = 'Class {}'.format(cl)
+        ax.set_title(title)
+        axg = scatter_kdpg(kdpg, t)
+        axg.set_title(title)
--- scripts/vertical/scr_load_cases.py
+++ scripts/vertical/scr_load_cases.py
@@ -10,6 +10,6 @@ if __name__ == '__main__':
     cases_id = 'snow'
     rain_season = cases_id in ('rain',)
     flag = 'ml_ok' if rain_season else None
-    c = multicase.MultiCase.from_caselist(cases_id, filter_flag=flag, has_ml=rain_season)
+    cc = multicase.MultiCase.from_caselist(cases_id, filter_flag=flag, has_ml=rain_season)
     name = conf.SCHEME_ID_RAIN if rain_season else conf.SCHEME_ID_SNOW
-    c.load_classification(name)
+    cc.load_classification(name)
