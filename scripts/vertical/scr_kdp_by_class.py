6507832b77a03c92f0a6020658633d755323d9dc
Jussi Tiira <jussi.tiira@helsinki.fi>
2019-05-27 16:11:05 +0300

    SCR: make nice plots

--- scripts/vertical/scr_kdp_by_class.py
+++ scripts/vertical/scr_kdp_by_class.py
@@ -1,14 +1,26 @@
 # coding: utf-8
 """Visualize kdp vs t_s by class"""
+from os import path
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 from radcomp.visualization import LABELS
+from radcomp.vertical import RESULTS_DIR
 from j24.visualization import scatter_kde
+from j24 import ensure_join
 
 
-def scatter_kdp(kdp, t):
-    fig, ax = plt.subplots()
+def hlines(ax):
+    """hlines at important temperatures"""
+    ax.axhline(-3, color='black')
+    ax.axhline(-8, color='black')
+    ax.axhline(-10, color='black')
+    ax.axhline(-20, color='black')
+
+
+def scatter_kdp(kdp, t, ax=None):
+    ax = ax or plt.gca()
     selection = ~np.isnan(kdp.values.flatten())
     kdp_flat = kdp.values.flatten()[selection]
     t_flat = t.values.flatten()[selection]
@@ -18,11 +30,12 @@ def scatter_kdp(kdp, t):
     ax.invert_yaxis()
     ax.set_ylabel(LABELS['T'])
     ax.set_xlabel(LABELS['KDP'])
-    return ax
+    hlines(ax)
+    return fig, ax
 
 
-def scatter_kdpg(kdpg, t):
-    fig, ax = plt.subplots()
+def scatter_kdpg(kdpg, t, ax=None):
+    ax = ax or plt.gca()
     selection = ~np.isnan(kdpg.values.flatten())
     kdpg_flat = kdpg.values.flatten()[selection]
     t_flat = t.values.flatten()[selection]
@@ -32,17 +45,23 @@ def scatter_kdpg(kdpg, t):
     ax.invert_yaxis()
     ax.set_ylabel(LABELS['T'])
     ax.set_xlabel(LABELS['KDPG'])
+    ax.axvline(0, color='black')
+    hlines(ax)
     return ax
 
 
 if __name__ == '__main__':
-    plt.ion()
+    plt.ioff()
     for cl in range(cc.vpc.n_clusters):
+        fig, axarr = plt.subplots(1, 2, figsize=(10, 5))
         t = cc.data['T'].loc[:, cc.classes()==cl]
         kdp = cc.data.kdp.loc[:, cc.classes()==cl]
         kdpg = cc.data.kdpg.loc[:, cc.classes()==cl]
-        ax = scatter_kdp(kdp, t)
+        scatter_kdp(kdp, t, ax=axarr[0])
         title = 'Class {}'.format(cl)
-        ax.set_title(title)
-        axg = scatter_kdpg(kdpg, t)
-        axg.set_title(title)
+        scatter_kdpg(kdpg, t, ax=axarr[1])
+        fig.suptitle(title)
+        outdir = ensure_join(RESULTS_DIR, 'kdp-t_scatter')
+        outfile = path.join(outdir, 'cl{}.png'.format(cl))
+        fig.savefig(outfile, bbox_inches='tight')
+        plt.close(fig)
