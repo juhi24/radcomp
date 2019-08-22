0fe60c946d750be5dc84565bc708a777342f18ae
Jussi Tiira <jussi.tiira@helsinki.fi>
2019-08-22 16:15:24 +0300

    SCR: minor

--- scripts/vpr/scr_azs.py
+++ scripts/vpr/scr_azs.py
@@ -12,7 +12,9 @@ if __name__ == '__main__':
     data = azs.load_series()
     types = {'class': int, 'azs': float}
     azs_cl = pd.concat([cc_s.classes(), data], axis=1).dropna().astype(types)
-    azs_cl.boxplot(column='class')
+    fig, axarr, _ = cc_r.plot_cluster_centroids(fields=['zh'], plot_counts=False, n_extra_ax=2)
+    azs_cl.boxplot(by='class', ax=axarr[-2])
     g = azs_cl.groupby('class')
+    g.size().plot.bar(ax=axarr[-1])
 
 
