9e70b2059707d97ba45f13558e5bd55c01ba256d
Jussi Tiira <jussi.tiira@helsinki.fi>
2019-10-11 17:56:44 +0300

    SCR: minor edits

--- scripts/vertical/scr_plot_rhi.py
+++ scripts/vertical/scr_plot_rhi.py
@@ -8,13 +8,13 @@ from j24 import home
 
 
 if __name__ == '__main__':
-    plt.close('all')
-    rawfile = path.join(home(), 'DATA', '201402212355_IKA.RHI_HV.raw')
+    #plt.close('all')
+    rawfile = path.join(home(), 'DATA', '201402152010_IKA.RHI_HV.raw')
     outfile = path.join(RESULTS_DIR, 'rhi_sample.png')
     radar = pyart.io.read(rawfile)
     fig, ax = plt.subplots()
     display = pyart.graph.RadarDisplay(radar)
-    display.plot('reflectivity')
+    display.plot('differential_reflectivity', vmin=-0.25, vmax=2)
     ax.set_ylim(bottom=0, top=10)
     ax.set_xlim(left=0, right=80)
     fig.savefig(outfile)
